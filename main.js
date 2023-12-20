298c1fe13a5beec256ecc94a48cc1ca2ae94db4b
JDSdamasceno <jadersondamascenodelima@gmail.com>
2023-12-20 20:58:12 -0300

    construção das validações dos campos em andamento

--- main.js
+++ main.js
@@ -5,18 +5,24 @@ const calculateButton = document.getElementById("calculate-results");
 
 function renderProgression(evt) {
   evt.preventDefault();
-  const startingAmount = Number(form["starting-amount"].value);
+  const startingAmount = Number(
+    form["starting-amount"].value.replace(",", ".")
+  );
   // const startingAmount = Number(
   //   document.getElementById("starting-amount").value
   // );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRatePeriod = document.getElementById("evaluation-period").value;
+  const returnRatePeriod = document
+    .getElementById("evaluation-period")
+    .value.replace(",", ".");
   const returnRate = Number(document.getElementById("return-rate").value);
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -28,6 +34,17 @@ function renderProgression(evt) {
   );
 
   console.log(returnsArray);
+}
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
 }
 
 // form.addEventListener("submit", renderProgression);
